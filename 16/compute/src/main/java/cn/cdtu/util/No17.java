aaf2960428c48bb7f8ee0d7d976eb4bafdfe4c5c
root <736225121@qq.com>
2022-09-30 20:38:43 +0800

    完成17，18（马昆已经的算法已经完成了18），19，20（没明白需求的意思，），21、22、23这三个需求直接改变No17类generateAddAndSUB方法中的numCol数值就可以满足。

---
+++ 16/compute/src/main/java/cn/cdtu/util/No17.java
@@ -0,0 +1,46 @@
+package cn.cdtu.util;
+
+
+import cn.cdtu.pojo.Equation;
+import cn.cdtu.service.impl.ExerciseServiceImpl;
+
+//需求17
+public class No17 {
+
+    ExerciseServiceImpl service = new ExerciseServiceImpl();
+    //加减混合输出
+    public void generateAddAndSUB(int total,int numCol) {
+        for (int i = 1; i <= total; i++) {
+            Equation equation;
+            Object[] objects;
+            if(i < (total/2)){
+                equation = service.generateAdd(0, 100, 3);
+                objects = equation.operands.toArray();
+                String num1 = objects[0].toString();
+                String num2 = objects[1].toString();
+                String num3 = objects[2].toString();
+
+                if (i % numCol ==0){
+                    System.out.print("\t" + num1 + "+" + num2 + "+" + num3 + "=" + equation.getValue()+"\n");
+                }else {
+                    System.out.print("\t" + num1 + "+" + num2 + "+" + num3 + "=" + equation.getValue()+"\t");
+                }
+            }else {
+                equation = service.generateSub(0, 100, 3);
+                objects = equation.operands.toArray();
+                String num1 = objects[0].toString();
+                String num2 = objects[1].toString();
+                String num3 = objects[2].toString();
+
+                if (i % numCol ==0){
+                    System.out.print("\t" + num1 + "-" + num2 + "-" + num3 + "=" + equation.getValue()+"\n");
+                }else {
+                    System.out.print("\t" + num1 + "-" + num2 + "-" + num3 + "=" + equation.getValue()+"\t");
+                }
+            }
+
+
+        }
+
+    }
+}
